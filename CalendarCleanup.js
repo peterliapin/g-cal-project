1551019b0e179ebf62582bbea825c62a361c4034
Peter Liapin <peter@liapin.space>
2025-01-23 23:57:58 +0530

    preserveEventsWithExemptedAttendees = false by default

--- CalendarCleanup.js
+++ CalendarCleanup.js
@@ -1,7 +1,7 @@
 const fromDate = new Date(2025, 1, 10); // February 3, 2025
 const exemptedUsers = []; // Add emails for exempted users
 const testRun = true; // Set to true for testing without applying changes
-const preserveEventsWithExemptedAttendees = true; // Preserve events with exempted attendees if true
+const preserveEventsWithExemptedAttendees = false; // Preserve events with exempted attendees if true
 const restartFromBeginning = false; // Set to true to restart from the beginning
 
 function readAndCancelEvents() {
