e82b03c2f641bfff3839391853720a58d0a4f062
Peter Liapin <peter@liapin.space>
2025-01-23 23:59:07 +0530

    Update CalendarCleanup.js

--- CalendarCleanup.js
+++ CalendarCleanup.js
@@ -1,4 +1,4 @@
-const fromDate = new Date(2025, 1, 10); // February 3, 2025
+const fromDate = new Date(2025, 1, 10); // February 10, 2025
 const exemptedUsers = []; // Add emails for exempted users
 const testRun = true; // Set to true for testing without applying changes
 const preserveEventsWithExemptedAttendees = false; // Preserve events with exempted attendees if true
